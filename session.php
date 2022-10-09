6340b01fa20a3e064755955a29d7c563c4379b08
Ahmad Junaidisk <94750834+AhmadJunaidisk@users.noreply.github.com>
2022-10-09 23:07:01 +0700

    Update session.php

--- session.php
+++ session.php
@@ -2,92 +2,33 @@
 
 class Session {
 
-    private $Skey = [];
-    private $SVal = [];
+    private static $Skey = [];
+    private static $SVal = [];
 
-    function __Construct() {
-        session_start();
-    }
-
-    public function Buat(array $arg) {
+    public static function Buat(array $arg) {
         if(is_array($arg)) {
             foreach(array_keys($arg) as $key) {
-                $this->Skey[] = $key;
+                self::$Skey[] = $key;
             } foreach($arg as $key) {
-                $this->SVal[] = $key;
-            } for($i=0; $i<count($this->Skey); $i++) {
-                $_SESSION[$this->Skey[$i]] = $this->SVal[$i];
+                self::$SVal[] = $key;
+            } for($i=0; $i<count(self::$Skey); $i++) {
+                $_SESSION[self::$Skey[$i]] = self::$SVal[$i];
             } 
         } else {
             $_SESSION[$arg] = $arg;
         }
-        return $this;
-    }
-
-    public function Cek() {
-        header("Content-type: Application/json");
-        if(empty($_SESSION)) {
-            Err::Respon([
-                "data" => [
-                    "status" => 404,
-                    "pesan" => "Session kosong"
-                ]
-            ]);
-        } else {
-            echo json_encode($_SESSION, JSON_PRETTY_PRINT);
-        }
-    }
-
-    public function Unset(string $arg) {
-        unset($_SESSION[$arg]);
-    }
-
-    public function Hapus() {
-        $_SESSION = [];
-        session_destroy();
+        return new static;
     }
 
     public function Encrypt() {
-        for($i=0; $i < count($this->SVal); $i++) {
-            $_SESSION[$this->Skey[$i]] = md5($this->SVal[$i]);
+        for($i=0; $i < count(self::$SVal); $i++) {
+            if(is_bool($_SESSION[self::$Skey[$i]])) {
+                $_SESSION[self::$Skey[$i]] = self::$SVal[$i];
+            } else {
+                $_SESSION[self::$Skey[$i]] = md5(self::$SVal[$i]);
+            }
         }
     }
 
 }
-
-class Err {
-
-    public static function Respon(array $arg) {
-        header("Content-type: Application/json");
-        echo json_encode($arg, JSON_PRETTY_PRINT);
-    }
-
-}
-
-/** 
-* Membuat Session lebih dari satu
-*/
-(new Session)->Buat([
-  "username" => "Ahmad Junaidi",
-  "status" => "online"
-]);
-
-/** 
-* Membuat Session lebih dari satu sekaligus terenskripsi md5
-*/
-(new Session)->Buat([
-  "username" => "Ahmad Junaidi",
-  "status" => "online"
-])->Encrypt();
-
-/** 
-* Menghapus session dengan method hapus ( ini sama dengan destroy() )
-*/
-(new Session)->Hapus()
-
-/** 
-* Menghapus session dengan method unset ( ini sama dengan unset() )
-*/
-(new Session)->unset("username");
-
 ?>
